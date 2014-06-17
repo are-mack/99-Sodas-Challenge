33252a8925cbf828c962222450860e572a3a54d0
McCawley <McCawley@ryMacBP.local>
2014-06-17 09:08:14 -0400

    Initial Commit

---
+++ 99 Sodas Challenge/AppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  AppDelegate.h
+//  99 Sodas Challenge
+//
+//  Created by McCawley on 6/17/14.
+//  Copyright (c) 2014 Ryan McCawley. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
